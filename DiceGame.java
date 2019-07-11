2c2ca8f092a57fab03b47a185347380538bfb366
s1260033 <s1260033@lsrdc8>
2019-07-11 14:59:29 +0900

    Added determining number of thrown dices

--- DiceGame.java
+++ DiceGame.java
@@ -28,7 +28,7 @@ class Die {
     }
 
     void roll() {
-	this.faceValue = r.nextInt(5) + 1;
+	this.faceValue = r.nextInt(6) + 1;
     }
 
     int getFaceValue() {
@@ -43,11 +43,15 @@ class DiceGame {
     public static void main(String[] args) {
 	Scanner s = new Scanner(System.in);
 
-	int i = 0, sum = 0;
-	Die[]  di = {
-	    new Die(),
-	    new Die(),
-	};
+	int i = 0, sum = 0, flag = 0;
+	Die[]  di;
+	Random r = new Random();
+
+
+	di = new Die[r.nextInt(5)+2];
+	for(i = 0 ; i < di.length ; i++) {
+	    di[i] = new Die();
+	}
 	
 	System.out.println("What is your name?");
 	System.out.print("> ");
@@ -56,25 +60,35 @@ class DiceGame {
 	p.Greeting();
 	
 	System.out.println("Rolling the dice . . .");
+	System.out.printf("Die 1: %d\n",di.length);
+	System.out.printf("You can throw %d dices\n",di.length);
 
-
+	
 	for(i = 0 ; i < di.length ; i++) {
 	    System.out.printf("Die %d: %d\n",i+1,di[i].getFaceValue());
 	    sum += di[i].getFaceValue();
 	}
-	
-	if(di[0].getFaceValue() == di[1].getFaceValue()) {
+
+	for(i = 1 ; i < di.length ; i++) {
+	    if(di[0].getFaceValue() != di[i].getFaceValue()) {
+		flag = 0;
+		break;
+	    }
+	    else flag = 1;
+	}
+
+	if(flag == 1) {
 	    System.out.println("Dices show the same number!");
-	    System.out.println("You can play the dice again!");
+	    System.out.println("You can throw the dice again!");
 	    Die chance = new Die();
-	    System.out.printf("Die 3: %d\n",chance.getFaceValue());
+	    System.out.printf("Die %d: %d\n",di.length + 1, chance.getFaceValue());
 	    sum += chance.getFaceValue();
 	}
-	else sum = sum;
+	else flag = 0;
 
 	System.out.printf("Total value: %d\n",sum);
 
-	if(sum > 7) System.out.println(p.getName() + " won!");
+	if(sum > (6*di.length) / 2) System.out.println(p.getName() + " won!");
 	else System.out.println(p.getName() + " lost");
     }  
 }
